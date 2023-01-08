2435b33bd4cf1239568ab80192de9a349ab31325
fzhulitov <fzhulitov@gmail.com>
2023-01-08 12:09:39 +0200

    Recovering after changing boi Site

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import boi
 
-abc = boi.kr.get_ir()
-print(abc)
-
 abc = boi.gdp.get_gdp()
 print(abc)
 
 abc = boi.infl.get_cp()
 print(abc)
+
+abc = boi.kr.get_ir()
+print(abc)
+
+
+
+
 
 abc = boi.infl.get_inflation()
 print(abc)
