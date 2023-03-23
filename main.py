c5376f4a009dcb4291dd05ef52aabeaf60067dee
Sergey Kikevich <sergey@rostsber.ru>
2023-03-23 10:21:58 +0300

    refactor: few changes

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import boi
 
-abc = boi.gdp.get_gdp(date_start='2000-01-01')
-print(abc)
-
-# abc = boi.infl.get_cpi()
+# abc = boi.gdp.get_gdp(date_start='2000-01-01')
 # print(abc)
+
+abc = boi.infl.get_cpi(date_start='2022-01-01')
+print(abc)
 
 abc = boi.kr.get_ir(date_start='2022-01-01')
 print(abc)
 
-# abc = boi.infl.get_inflation()
-# print(abc)
+abc = boi.infl.get_inflation(date_start='2022-01-01')
+print(abc)
