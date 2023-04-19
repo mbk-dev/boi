ab85c9c5e64f4f750f480e8f1e7e06876f03c508
Sergey Kikevich <sergey@rostsber.ru>
2023-04-19 11:26:57 +0300

    fix: actualize CPI URL

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import boi
 # abc = boi.gdp.get_gdp(date_start='2000-01-01')
 # print(abc)
 
-abc = boi.infl.get_cpi(date_start='2022-01-01')
-print(abc)
+# abc = boi.infl.get_cpi(date_start='2022-01-01')
+# print(abc)
+#
+# abc = boi.kr.get_ir(date_start='2022-01-01')
+# print(abc)
 
-abc = boi.kr.get_ir(date_start='2022-01-01')
-print(abc)
-
-abc = boi.infl.get_inflation(date_start='2022-01-01')
+abc = boi.infl.get_inflation(date_start='2021-01-01')
 print(abc)
