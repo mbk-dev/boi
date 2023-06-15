891d7875feb7340405330f953b2c4514e46d33b1
Sergey Kikevich <sergey@rostsber.ru>
2023-06-15 18:54:57 +0300

    fix: some data returned as DF not Series

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import boi
 
-# abc = boi.gdp.get_gdp(date_start='2000-01-01')
-# print(abc)
+abc = boi.gdp.get_gdp(date_start='2000-01-01')
+print(abc)
 
-# abc = boi.infl.get_cpi(date_start='2022-01-01')
-# print(abc)
-#
-# abc = boi.kr.get_ir(date_start='2022-01-01')
-# print(abc)
+abc = boi.infl.get_cpi(date_start='2022-01-01')
+print(abc)
+
+abc = boi.kr.get_ir(date_start='2022-01-01')
+print(abc)
 
 abc = boi.infl.get_inflation(date_start='2021-01-01')
-print(abc)
+print(abc * 100)
